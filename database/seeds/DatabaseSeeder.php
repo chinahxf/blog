7334ac5a80a6ea9458bec1007ae6c24bb9dbf6d2
fei <chinahxf@163.com>
2018-08-01 07:34:11 +0800

    清空文章

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-         $this->call(ArticleTableSeeder::class);
+//         $this->call(ArticleTableSeeder::class);
 //         $this->call(CommentTableSeeder::class);
          $this->call(CategoryTableSeeder::class);
     }
