39df14dd04edfff8d70622dea6792714a43566fe
fei <chinahxf@163.com>
2018-09-07 07:03:47 +0800

    百度站长工具连接推送

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -16,6 +16,20 @@
     <link rel="stylesheet" href="{{asset('css/portal/swiper-4.2.6.min.css')}}">
     @yield('portal_style')
     <script>
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https'){
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else{
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+    </script>
+    <script>
         var _hmt = _hmt || [];
         (function () {
             var hm = document.createElement("script");
